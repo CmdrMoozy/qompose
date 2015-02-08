627f399722e69412db4f358c56dfab3f9c5b3eea
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-02-08 08:28:36 -0700

    Improve the CMake code for configuring compile flags.

--- CMake/SetFlags.cmake
+++ CMake/SetFlags.cmake
@@ -1,4 +1,4 @@
-function(qomposeEnableWarnings)
+function(qomposeSetCompileFlags)
 	if(CMAKE_CXX_COMPILER_ID MATCHES GNU)
 		set(F_WARN "-pedantic -Wall -Wextra -Wcast-align -Wcast-qual")
 		set(F_WARN "${F_WARN} -Wctor-dtor-privacy -Wlogical-op")
@@ -12,19 +12,12 @@ function(qomposeEnableWarnings)
 		set(F_WARN "${F_WARN} -Wzero-as-null-pointer-constant")
 		set(F_WARN "${F_WARN} -Wstrict-overflow=5 -Wswitch-default")
 
-		set(F_DBG "${F_WARN} -std=c++11 -O0")
-		set(F_REL "${F_WARN} -std=c++11 -fomit-frame-pointer")
-
-		set(CMAKE_CXX_FLAGS_DEBUG
-			"${CMAKE_CXX_FLAGS_DEBUG} ${F_DBG}"
-			CACHE INTERNAL "CMAKE_CXX_FLAGS_RELEASE")
-		set(CMAKE_CXX_FLAGS_RELEASE
-			"${CMAKE_CXX_FLAGS_RELEASE} ${F_REL}"
-			CACHE INTERNAL "CMAKE_CXX_FLAGS_RELEASE")
+		set(F_DBG "${F_WARN} -std=c++11 -g -O0")
+		set(F_REL "${F_WARN} -std=c++11 -O2 -fomit-frame-pointer")
 	elseif(CMAKE_CXX_COMPILER_ID MATCHES Clang)
 		set(F_WARN "-pedantic -Wall -Wextra -Wcast-align -Wcast-qual")
 		set(F_WARN "${F_WARN} -Wdisabled-optimization -Wformat=2")
-		set(F_WARN "${F_WARN} -Winit-self -Wstrict-overflows=5")
+		set(F_WARN "${F_WARN} -Winit-self -Wstrict-overflow=5")
 		set(F_WARN "${F_WARN} -Wmissing-include-dirs -Wold-style-cast")
 		set(F_WARN "${F_WARN} -Wshadow -Wswitch-default -Weffc++")
 		set(F_WARN "${F_WARN} -Woverloaded-virtual -Wredundant-decls")
@@ -32,25 +25,17 @@ function(qomposeEnableWarnings)
 		set(F_WARN "${F_WARN} -Wint-to-void-pointer-cast")
 		set(F_WARN "${F_WARN} -Wctor-dtor-privacy")
 
-		set(F_DBG "${F_WARN} -std=c++11 -O0")
-		set(F_REL "${F_WARN} -std=c++11 -fomit-frame-pointer")
-
-		set(CMAKE_CXX_FLAGS_DEBUG
-			"${CMAKE_CXX_FLAGS_DEBUG} ${F_DBG}"
-			CACHE INTERNAL "CMAKE_CXX_FLAGS_RELEASE")
-		set(CMAKE_CXX_FLAGS_RELEASE
-			"${CMAKE_CXX_FLAGS_RELEASE} ${F_REL}"
-			CACHE INTERNAL "CMAKE_CXX_FLAGS_RELEASE")
+		set(F_DBG "${F_WARN} -std=c++11 -g -O0")
+		set(F_REL "${F_WARN} -std=c++11 -O2 -fomit-frame-pointer")
 	elseif(CMAKE_CXX_COMPILER_ID MATCHES MSVC)
 		set(F_DBG "/W4 /WX /FIQomposeCommon/MSVCWarnings.h")
 
 		set(F_REL "/W4 /WX /FIQomposeCommon/MSVCWarnings.h")
-
-		set(CMAKE_CXX_FLAGS_DEBUG
-			"${CMAKE_CXX_FLAGS_DEBUG} ${F_DBG}"
-			CACHE INTERNAL "CMAKE_CXX_FLAGS_RELEASE")
-		set(CMAKE_CXX_FLAGS_RELEASE
-			"${CMAKE_CXX_FLAGS_RELEASE} ${F_REL}"
-			CACHE INTERNAL "CMAKE_CXX_FLAGS_RELEASE")
+	else()
+		set(F_DBG "")
+		set(F_REL "")
 	endif()
+
+	set(CMAKE_CXX_FLAGS_RELEASE "${F_REL}" PARENT_SCOPE)
+	set(CMAKE_CXX_FLAGS_DEBUG "${F_DBG}" PARENT_SCOPE)
 endfunction()
