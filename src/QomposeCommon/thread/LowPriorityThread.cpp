9b0e675facb0133f8faa37f142ae9ad2f1a8c569
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-04-05 13:02:27 -0600

    Add a low priority thread implementation.

---
+++ src/QomposeCommon/thread/LowPriorityThread.cpp
@@ -0,0 +1,145 @@
+/*
+ * Qompose - A simple programmer's text editor.
+ * Copyright (C) 2013 Axel Rasmussen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "LowPriorityThread.h"
+
+#include <stdexcept>
+#include <system_error>
+
+#include "QomposeCommon/thread/Utils.h"
+#include "QomposeCommon/util/Strings.h"
+
+#ifdef __linux__
+#include <pthread.h>
+#include <sched.h>
+#endif
+
+namespace
+{
+/*!
+ * This function sets the calling thread's CPU priority to its lowest value.
+ */
+void setLowCPUPriority()
+{
+#if defined(_WIN32)
+	qompose::thread_utils::setThreadPriority(PROCESS_MODE_BACKGROUND_BEGIN);
+#elif defined(__linux__)
+	struct sched_param param;
+	param.sched_priority = sched_get_priority_min(SCHED_OTHER);
+
+	int ret = pthread_setschedparam(pthread_self(), SCHED_OTHER, &param);
+	if(ret != 0)
+	{
+		throw std::runtime_error(qompose::string_utils::getErrnoError(
+		        "Setting thread CPU priority failed.", ret));
+	}
+#else
+	static_assert(false, "CPU priority unsupported on this platform.");
+#endif
+}
+
+/*!
+ * This function sets the calling thread's IO priority to its lowest value.
+ */
+void setLowIOPriority()
+{
+#if defined(_WIN32)
+	qompose::thread_utils::setThreadPriority(PROCESS_MODE_BACKGROUND_BEGIN);
+#elif defined(__linux__)
+	qompose::thread_utils::ioprio_set(
+	        qompose::thread_utils::IOPrioWho::Process, 0,
+	        qompose::thread_utils::ioprioPrioValue(
+	                qompose::thread_utils::IOPrioClass::Idle, 0));
+#else
+	static_assert(false, "IO priority unsupported on this platform.");
+#endif
+}
+
+/*!
+ * This function sets the calling thread's priority to its lowest value for
+ * all of the given priority types.
+ *
+ * \param types The types of priority to set.
+ */
+void setLowPriority(const std::initializer_list<qompose::PriorityType> &types)
+{
+	for(const auto &type : types)
+	{
+		switch(type)
+		{
+		case qompose::PriorityType::CPU:
+			setLowCPUPriority();
+			break;
+
+		case qompose::PriorityType::IO:
+			setLowIOPriority();
+			break;
+
+		default:
+			throw std::runtime_error("Invalid PriorityType.");
+		}
+	}
+}
+}
+
+namespace qompose
+{
+LowPriorityThread::LowPriorityThread(
+        const std::initializer_list<PriorityType> &t,
+        const std::function<void()> &f)
+        : thread()
+{
+	auto function = [t, f]()
+	{
+		setLowPriority(t);
+		f();
+	};
+	thread.reset(new std::thread(function));
+}
+
+LowPriorityThread::LowPriorityThread(PriorityType t,
+                                     const std::function<void()> &f)
+        : LowPriorityThread(std::initializer_list<PriorityType>({t}), f)
+{
+}
+
+LowPriorityThread::~LowPriorityThread()
+{
+	join();
+}
+
+std::thread::id LowPriorityThread::getID() const
+{
+	return thread->get_id();
+}
+
+void LowPriorityThread::join()
+{
+	try
+	{
+		thread->join();
+	}
+	catch(const std::system_error &e)
+	{
+		// If the thread isn't joinable, it probably just stopped on
+		// its own, so we eat the resulting exception.
+		if(e.code() != std::errc::invalid_argument)
+			throw;
+	}
+}
+}
---
+++ src/QomposeCommon/thread/LowPriorityThread.h
@@ -0,0 +1,98 @@
+/*
+ * Qompose - A simple programmer's text editor.
+ * Copyright (C) 2013 Axel Rasmussen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_QOMPOSECOMMON_THREAD_LOW_PRIORITY_THREAD_H
+#define INCLUDE_QOMPOSECOMMON_THREAD_LOW_PRIORITY_THREAD_H
+
+#include <functional>
+#include <initializer_list>
+#include <memory>
+#include <thread>
+
+namespace qompose
+{
+/*!
+ * \brief Defines the types of thread priority which can be changed.
+ */
+enum class PriorityType
+{
+	CPU,
+	IO
+};
+
+/*!
+ * \brief This class implements a low-priority thread.
+ *
+ * This class can be used to execute some code in a thread which is marked as
+ * having low CPU or IO priority, so as to not interfere with other processes
+ * on the system.
+ *
+ * It's worth noting that the priority of this thread generally can't be raised
+ * without superuser permissions, so now API is provided to do so.
+ */
+class LowPriorityThread
+{
+public:
+	/*!
+	 * This constructor creates a new low priority thread, which has low
+	 * priority for all of the given priority types.
+	 *
+	 * \param t The low priority types for this thread.
+	 * \param f The function for this thread to execute.
+	 */
+	explicit LowPriorityThread(const std::initializer_list<PriorityType> &t,
+	                           const std::function<void()> &f);
+
+	/*!
+	 * This constructor creates a new low priority thread, which has low
+	 * priority for the given priority type.
+	 *
+	 * \param t The low priority type for this thread.
+	 * \param f The function for this thread to execute.
+	 */
+	LowPriorityThread(PriorityType t, const std::function<void()> &f);
+
+	LowPriorityThread(const LowPriorityThread &) = delete;
+
+	/*!
+	 * This destructor automatically join()'s the thread to ensure it stops
+	 * before destructing.
+	 */
+	~LowPriorityThread();
+
+	LowPriorityThread &operator=(const LowPriorityThread &) = delete;
+
+	/*!
+	 * This function returns this thread's ID.
+	 *
+	 * \return This thread's ID.
+	 */
+	std::thread::id getID() const;
+
+	/*!
+	 * This function waits for this thread to stop running. If something
+	 * goes wrong, an exception will be thrown.
+	 */
+	void join();
+
+private:
+	std::unique_ptr<std::thread> thread;
+};
+}
+
+#endif
