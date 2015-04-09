aa36404898ac4abc68f35faf305892391ebb3e8f
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-04-08 20:17:42 -0600

    Add utility functions for safely creating new Git objects.

--- src/QomposeCommon/git/Utils.h
+++ src/QomposeCommon/git/Utils.h
@@ -19,7 +19,10 @@
 #ifndef INCLUDE_QOMPOSECOMMON_GIT_UTILS_H
 #define INCLUDE_QOMPOSECOMMON_GIT_UTILS_H
 
+#include <functional>
+#include <stdexcept>
 #include <string>
+#include <utility>
 
 namespace qompose
 {
@@ -42,6 +45,76 @@ std::string lastErrorToString();
  * \return The path to the repository containing the given path.
  */
 std::string discoverRepository(const std::string &p);
+
+/*!
+ * This function constructs a new Git object of any type using the given
+ * creator and deleter functions. This function guarantees that either a valid
+ * pointer will be returned, or an exception will be thrown, and that there is
+ * no possibility for leaking memory prior to returning.
+ *
+ * \param creator The creator function to use.
+ * \param deleter The deleter function to use.
+ * \param args The extra arguments to pass to the creator function.
+ * \return The newly created Git object.
+ */
+template <typename object_t, typename... args_t>
+object_t *
+newGitObject(const std::function<int(object_t **, args_t...)> &creator,
+             const std::function<void(object_t *)> &deleter, args_t... args)
+{
+	object_t *obj = nullptr;
+
+	try
+	{
+		int ret = creator(&obj, std::forward<args_t>(args)...);
+		if(ret != 0)
+			throw std::runtime_error(lastErrorToString());
+	}
+	catch(...)
+	{
+		if(obj != nullptr)
+			deleter(obj);
+
+		throw;
+	}
+
+	return obj;
+}
+
+/*!
+ * This function is a wapper for the main newGitObject() overload, which allows
+ * a function pointer to be used as a creator.
+ *
+ * \param creator The creator function to use.
+ * \param deleter The deleter function to use.
+ * \param args The extra arguments to pass to the creator function.
+ * \return The newly created Git object.
+ */
+template <typename object_t, typename... args_t>
+object_t *
+newGitObject(const std::function<int(object_t **, args_t...)> &creator,
+             void (&deleter)(object_t *), args_t... args)
+{
+	return newGitObject(creator, std::function<void(object_t *)>(deleter),
+	                    args...);
+}
+
+/*!
+ * This function is a wapper for the main newGitObject() overload, which allows
+ * a function pointer to be used as both the creator and deleter.
+ *
+ * \param creator The creator function to use.
+ * \param deleter The deleter function to use.
+ * \param args The extra arguments to pass to the creator function.
+ * \return The newly created Git object.
+ */
+template <typename object_t, typename... args_t>
+object_t *newGitObject(int (&creator)(object_t **, args_t...),
+                       void (&deleter)(object_t *), args_t... args)
+{
+	return newGitObject(std::function<int(object_t **, args_t...)>(creator),
+	                    std::function<void(object_t *)>(deleter), args...);
+}
 }
 }
 
