9367c31e2c1cba6ea5f8f26fcb5ae60c63ec7d20
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-07-03 00:01:31 -0600

    Split decreaseSelectionIndent into multiple functions.

--- src/QomposeCommon/editor/algorithm/Indentation.cpp
+++ src/QomposeCommon/editor/algorithm/Indentation.cpp
@@ -68,6 +68,97 @@ SelectionIndentationState getSelectionState(QTextCursor &cursor)
 
 	return state;
 }
+
+/*!
+ * This function performs the first step of decreaseSelectionIndent. Namely,
+ * we try to remove at most one full indentation string from the beginning of
+ * each block in the selection, and we return true if at least one block was
+ * modified in this way.
+ *
+ * \param cursor The cursor to operate on.
+ * \param state The cursor's original selection state.
+ * \param mode The indentation mode to use.
+ * \param width The indentation width to use.
+ * \return True if at least one block was successfully de-indented.
+ */
+bool deindentSelection(QTextCursor &cursor,
+                       SelectionIndentationState const &state,
+                       qompose::IndentationMode mode, std::size_t width)
+{
+	bool foundIndent = false;
+	QString indentStr =
+	        qompose::editor::algorithm::getIndentationString(mode, width);
+
+	for(int i = 0; i <= state.blockCount; ++i)
+	{
+		cursor.movePosition(QTextCursor::StartOfBlock,
+		                    QTextCursor::MoveAnchor);
+
+		// If this block starts with our indentation string, we want
+		// to remove that one instance of it from the block.
+		if(static_cast<std::size_t>(cursor.block().length()) >= width)
+		{
+			cursor.movePosition(QTextCursor::NextCharacter,
+			                    QTextCursor::KeepAnchor, width);
+		}
+
+		if(cursor.hasSelection() &&
+		   (cursor.selectedText() == indentStr))
+		{
+			cursor.removeSelectedText();
+			foundIndent = true;
+		}
+
+		cursor.movePosition(QTextCursor::NextBlock,
+		                    QTextCursor::MoveAnchor);
+	}
+	return foundIndent;
+}
+
+/*!
+ * This function performs the optional second step of decreaseSelectionIndent.
+ * If no blocks were successfully de-indented, this function will attempt to
+ * remove any leading whitespace from any of the lines. See the documentation
+ * for decreaseSelectionIndentation for more information on exactly what is
+ * done.
+ *
+ * \param cursor The cursor to operate on.
+ * \param state The cursor's original selection state.
+ * \param width The indentation width to use.
+ */
+void dewhitespaceSelection(QTextCursor &cursor,
+                           SelectionIndentationState const &state,
+                           std::size_t width)
+{
+	// Try to remove up to the indentation width's number of whitespace
+	// characters.
+	cursor.setPosition(state.startPosition, QTextCursor::MoveAnchor);
+	for(int i = 0; i <= state.blockCount; ++i)
+	{
+		cursor.movePosition(QTextCursor::StartOfBlock,
+		                    QTextCursor::MoveAnchor);
+
+		for(int j = 0; j < static_cast<int>(width); ++j)
+		{
+			QChar c = cursor.block().text().at(0);
+
+			// Stop at the first non-whitespace char.
+			if(!c.isSpace())
+				break;
+
+			// If this isn't the first char, and it's a
+			// tab instead of a normal space, stop here.
+			// We'll remove tabs on the next de-indent.
+			if((j > 0) && (c == '\t'))
+				break;
+
+			cursor.deleteChar();
+		}
+
+		cursor.movePosition(QTextCursor::NextBlock,
+		                    QTextCursor::MoveAnchor);
+	}
+}
 }
 
 namespace qompose
@@ -124,66 +215,9 @@ void decreaseSelectionIndent(QTextCursor &cursor, IndentationMode mode,
 		return;
 	SelectionIndentationState state = getSelectionState(cursor);
 
-	// Do the de-indent.
 	cursor.beginEditBlock();
-	bool foundIndent = false;
-	for(int i = 0; i <= state.blockCount; ++i)
-	{
-		cursor.movePosition(QTextCursor::StartOfBlock,
-		                    QTextCursor::MoveAnchor);
-
-		// If this block starts with our indentation string, we want
-		// to remove that one instance of it from the block.
-		if(static_cast<std::size_t>(cursor.block().length()) >= width)
-		{
-			cursor.movePosition(QTextCursor::NextCharacter,
-			                    QTextCursor::KeepAnchor, width);
-		}
-
-		if(cursor.hasSelection() &&
-		   (cursor.selectedText() == getIndentationString(mode, width)))
-		{
-			cursor.removeSelectedText();
-			foundIndent = true;
-		}
-
-		cursor.movePosition(QTextCursor::NextBlock,
-		                    QTextCursor::MoveAnchor);
-	}
-
-	if(!foundIndent)
-	{
-		// If no lines were indented, try to remove up to the
-		// indentation width's number of whitespace characters.
-		cursor.setPosition(state.startPosition,
-		                   QTextCursor::MoveAnchor);
-		for(int i = 0; i <= state.blockCount; ++i)
-		{
-			cursor.movePosition(QTextCursor::StartOfBlock,
-			                    QTextCursor::MoveAnchor);
-
-			for(int j = 0; j < static_cast<int>(width); ++j)
-			{
-				QChar c = cursor.block().text().at(0);
-
-				// Stop at the first non-whitespace char.
-				if(!c.isSpace())
-					break;
-
-				// If this isn't the first char, and it's a
-				// tab instead of a normal space, stop here.
-				// We'll remove tabs on the next de-indent.
-				if((j > 0) && (c == '\t'))
-					break;
-
-				cursor.deleteChar();
-			}
-
-			cursor.movePosition(QTextCursor::NextBlock,
-			                    QTextCursor::MoveAnchor);
-		}
-	}
-
+	if(!deindentSelection(cursor, state, mode, width))
+		dewhitespaceSelection(cursor, state, width);
 	cursor.endEditBlock();
 
 	// Set the cursor's selection to span all of the involved lines.
