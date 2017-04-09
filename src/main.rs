a74b482fed298e554df2d32a3a16d12e0b815587
Axel Rasmussen <axel.rasmussen1@gmail.com>
2017-04-08 18:02:28 -0700

    Add first pieces of terminal mode support.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[macro_use]
+extern crate error_chain;
+extern crate qompose;
+extern crate termios;
+
+use qompose::error::*;
+use std::io::{self, Read, Write};
+use std::os::unix::io::RawFd;
+use termios::Termios;
+
+const STDIN_FILENO: RawFd = 0;
+
+fn enable_raw_mode() -> Result<()> {
+    let mut raw = Termios::from_fd(STDIN_FILENO)?;
+
+    raw.c_lflag &= !termios::ECHO;
+
+    Ok(termios::tcsetattr(STDIN_FILENO, termios::TCSAFLUSH, &raw)?)
+}
+
+fn read_char_stdin() -> Result<char> {
+    Ok(char::from(match io::stdin().bytes().next() {
+        Some(c) => c?,
+        None => bail!("STDIN returned EOF"),
+    }))
+}
+
+fn run() -> Result<()> {
+    enable_raw_mode()?;
+
+    loop {
+        let c = read_char_stdin()?;
+        if c == 'q' {
+            break;
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        use error_chain::ChainedError;
+        let mut stderr = io::stderr();
+        writeln!(stderr, "{}", e.display()).expect("Error writing to STDERR");
+    }
 }
